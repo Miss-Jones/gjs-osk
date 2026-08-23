c0e50b139dcbf60f9947a9ebe751c8d069b3da55
JonasHR <jonasvhm@gmail.com>
2026-08-23 17:53:38 +0200

    Raise the keyboard within its actual parent instead of Main.uiGroup

--- gjsosk@vishram1123.com/extension.js
+++ gjsosk@vishram1123.com/extension.js
@@ -916,7 +916,7 @@ class Keyboard extends Dialog {
             this.inputDevice = Clutter.get_default_backend().get_default_seat().create_virtual_device(Clutter.InputDeviceType.KEYBOARD_DEVICE);
             this.state = State.OPENING
             this.show();
-            Main.uiGroup.set_child_above_sibling(this, null);
+            this.get_parent()?.set_child_above_sibling(this, null);
         }
         if (noPrep == null || noPrep) {
             if (major >= 48) {
@@ -957,7 +957,7 @@ class Keyboard extends Dialog {
                 })
             }
             this.opened = true;
-            Main.uiGroup.set_child_above_sibling(this, null);
+            this.get_parent()?.set_child_above_sibling(this, null);
             // [insert handwriting 5]
         }
     }
